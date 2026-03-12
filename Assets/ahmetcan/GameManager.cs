2e22dec6eaf0a150cc1509808e7a32c8ee220227
ggullagg <ahmetcamdu34@gmail.com>
2026-03-12 14:10:52 +0100

    game manager day night system first version finished

--- Assets/ahmetcan/GameManager.cs
+++ Assets/ahmetcan/GameManager.cs
@@ -1,16 +1,66 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //General Variables
+    private float Days = 1; // counting days for score panel
+    private float NightStep = 10; // after how much step the night gonna end
+    private float DayStep = 10; // after how much step the day gonna end
+    
+    //function variables
+    private float DayNightCounter = 0;
+    private bool Day = true;
+    
+    //timeline
+    float TimelineStart;
+    float TimelineTarget;
+    float TimelineDuration;
+    float TimelineTime;
+    bool TimelineAnimating;
+    
+    public Volume ppv;  // getting the volume
+
+    //DayNight Tween
+    public void StartTween(float newStart,float newTarget, float newDuration)
     {
-        
+        TimelineStart = newStart;
+        TimelineTarget = newTarget;
+        TimelineDuration = newDuration;
+        TimelineTime = 0;
+        TimelineAnimating = true;
     }
 
-    // Update is called once per frame
+    // player moved 1 time
+    void PlayerMoved()
+    {
+        if (!TimelineAnimating) {
+        DayNightCounter += 1;
+        if (Day &&  DayNightCounter >= DayStep) {
+            Day = false;
+            StartTween(0f,1f,5f);
+        } else if (!Day &&  DayNightCounter >= NightStep) {
+            Days += 1;
+            Day = true;
+            StartTween(1f,0f,5f);
+        } 
+        }
+    }
+
     void Update()
     {
-        
+        // Day Night Timeline (count)
+        if (!TimelineAnimating) return;
+        TimelineTime += Time.deltaTime;
+        float t = TimelineTime / TimelineDuration;
+        if (ppv)
+        {
+            ppv.weight = Mathf.Lerp(TimelineStart, TimelineTarget, t);
+            if (t >= 1f)
+            {
+                ppv.weight = TimelineTarget;
+                TimelineAnimating = false;
+            }
+        }
     }
 }
