b06157e4e3e0b2bab66c2a135fa9c701c02d44d4
ggullagg <ahmetcamdu34@gmail.com>
2026-03-16 12:19:09 +0100

    game over scene

---
+++ Assets/ahmetcan/GameOver.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOver : MonoBehaviour
+{
+  public void PlayGame()
+  {
+   Debug.Log("clicked yes");
+  }
+  
+  public void ReturnToMainMenu()
+  {
+      Debug.Log("clicked no");
+  }
+}
